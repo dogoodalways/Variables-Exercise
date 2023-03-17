9ccb005ba3650e855545b20995a3ff3a1073c796
yosefamiami@gmail.com <yosefamiami@gmail.com>
2023-03-17 18:34:46 -0400

    var exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Bob";
+            int age = 39;
+            char initial = 'B';
+            bool married = true;
+            double mitzvosCount = 23.87;
+            decimal chairtyMoney = 39;
+            Console.WriteLine($"Hi name is {name}. I am {age} years old. My parents named me Binakalapo but I changed it to my middle name so my middle initial is " +
+                $"{initial}. I am {married} and did {mitzvosCount}");
+
+
+
+
         }
     }
 }
